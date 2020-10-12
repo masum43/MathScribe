a0ca1d82a7ae0fae017ff26f8c636fab9df9e490
masum43 <masum.abir43@gmail.com>
2020-10-12 10:31:46 +0600

    Initial commit

---
+++ app/src/main/java/com/example/mathscribejava/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.mathscribejava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.github.kexanie.library.MathView;
+
+public class MainActivity extends AppCompatActivity {
+    MathView formula_two;
+    String tex = "This come from string. You can insert inline formula:" +
+            " \\(ax^2 + bx + c = 0\\) " +
+            "or displayed formula: $$\\sum_{i=0}^n i^2 = \\frac{(n^2+n)(2n+1)}{6}$$";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        formula_two = (MathView) findViewById(R.id.formula_two);
+        formula_two.setText(tex);
+    }
+}
